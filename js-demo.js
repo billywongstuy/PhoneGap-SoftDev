79cbfaab245887db1babb80f2986f667d90cad2f
Billy Wong <bwong5@stuy.edu>
2017-03-27 08:10:04 -0400

    changes

--- js-demo.js
+++ js-demo.js
@@ -22,12 +22,34 @@ var newText = document.getElementById("newText");
 
 var changeSize = function() {
     words.style.fontSize = newSize.value + "px";
-}
+};
 szChange.addEventListener("click",changeSize);
 
 
 var changeText = function() {
     console.log(newText.value);
     words.innerHTML = newText.value;
-}
+};
 txChange.addEventListener("click",changeText);
+
+
+/*---------
+DAYS
+----------*/
+
+var dayRes = document.getElementById("dayRes");
+var day = document.getElementById("day");
+
+var checkDay = function() {
+    console.log(dayRes.innerHTML);
+    var thinkDay = day.value;
+    if (day.value == new Date().getDay()) {
+	dayRes.innerHTML = "You are correct!";
+	console.log("corr");
+    }
+    else {
+	dayRes.innerHTML = "You are wrong!"
+    }
+}
+
+day.addEventListener("change",checkDay);
